207ad4aaf0b13c1f397778c185eed5a7e8ab23ed
Loz/Sea <78250007+Lozaine@users.noreply.github.com>
2025-03-23 23:41:46 +0800

    Update PlayerUtilities.java

--- src/main/java/com/lozaine/playerutilities/PlayerUtilities.java
+++ src/main/java/com/lozaine/playerutilities/PlayerUtilities.java
@@ -1,45 +1,101 @@
 package com.lozaine.playerutilities;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.lozaine.playerutilities.commands.FlyCommand;
-import com.lozaine.playerutilities.commands.GodCommand;
-import com.lozaine.playerutilities.commands.TeleportCommand;
-import com.lozaine.playerutilities.listeners.PlayerListener;
-import com.lozaine.playerutilities.managers.PlayerStateManager;
+public class PlayerUtilities extends JavaPlugin implements Listener {
 
-public class PlayerUtilities extends JavaPlugin {
-
-    private PlayerStateManager playerStateManager;
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
-        // Create configuration file if it doesn't exist
+        // Save default config if it doesn't exist
         saveDefaultConfig();
+        config = getConfig();
 
-        // Initialize player state manager
-        playerStateManager = new PlayerStateManager(this);
-
-        // Register event listeners
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+        // Register events
+        getServer().getPluginManager().registerEvents(this, this);
 
         // Register commands
+        getCommand("flyspeed").setExecutor(new FlySpeedCommand(this));
+        getCommand("worldtp").setExecutor(new WorldTeleportCommand(this));
         getCommand("fly").setExecutor(new FlyCommand(this));
         getCommand("god").setExecutor(new GodCommand(this));
-        getCommand("worldtp").setExecutor(new TeleportCommand(this));
 
         getLogger().info("PlayerUtilities has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Save player states before plugin disables
-        playerStateManager.saveAllPlayerStates();
         getLogger().info("PlayerUtilities has been disabled!");
     }
 
-    public PlayerStateManager getPlayerStateManager() {
-        return playerStateManager;
+    /**
+     * Listen for player join events to restore their previous fly/god mode status
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String playerUUID = player.getUniqueId().toString();
+
+        // Check if player had fly mode enabled previously
+        if (config.getBoolean("players." + playerUUID + ".fly", false)) {
+            player.setAllowFlight(true);
+            player.setFlying(true);
+            player.sendMessage(ChatColor.GREEN + "Your fly mode has been automatically restored.");
+        }
+
+        // Check if player had god mode enabled previously
+        if (config.getBoolean("players." + playerUUID + ".godmode", false)) {
+            // Enable god mode
+            enableGodMode(player);
+            player.sendMessage(ChatColor.GREEN + "Your god mode has been automatically restored.");
+        }
+
+        // Restore fly speed if it was customized
+        if (config.contains("players." + playerUUID + ".flyspeed")) {
+            float flySpeed = (float) config.getDouble("players." + playerUUID + ".flyspeed", 0.1);
+            player.setFlySpeed(flySpeed);
+        }
+    }
+
+    /**
+     * Enable god mode for a player
+     */
+    public void enableGodMode(Player player) {
+        player.setInvulnerable(true);
+        savePlayerState(player, "godmode", true);
+    }
+
+    /**
+     * Disable god mode for a player
+     */
+    public void disableGodMode(Player player) {
+        player.setInvulnerable(false);
+        savePlayerState(player, "godmode", false);
+    }
+
+    /**
+     * Save player state to config
+     */
+    public void savePlayerState(Player player, String property, Object value) {
+        String playerUUID = player.getUniqueId().toString();
+        config.set("players." + playerUUID + "." + property, value);
+        saveConfig();
+    }
+
+    /**
+     * Send a formatted message to a command sender
+     */
+    public void sendMessage(CommandSender sender, String message) {
+        String prefix = config.getString("messages.prefix", "&8[&bPlayerUtilities&8] &f");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
     }
 }
